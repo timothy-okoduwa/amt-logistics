a4b40ae8bc809b6be519803f87c3a5309cf14964
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2024-01-27 14:36:10 +0100

    first commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopName from '../pages/TopName';
+
+const Home = () => {
+  return (
+    <div>
+      <TopName />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/TopName.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TopName = () => {
+  return <div className="announcment">TopName</div>;
+};
+
+export default TopName;
